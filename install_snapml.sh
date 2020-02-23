579128876d3780c5ee3ed4485d728dd8e9b299cf
gilbertt78 <31360231+gilbertt78@users.noreply.github.com>
2020-02-23 11:03:33 +0800

    Create install_snapml.sh

---
+++ install_snapml.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#forked from https://github.com/nvgsg/rapids/blob/master/rapids-colab.sh
+set -eu
+if [ ! -f Miniconda2-latest-Linux-x86_64.sh ]; then
+    echo "Removing conflicting packages, will replace with RAPIDS compatible versions"
+
+    # install miniconda
+    echo "Installing conda"
+    wget https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    chmod +x Miniconda2-latest-Linux-x86_64.sh
+    bash Miniconda2-latest-Linux-x86_64.sh -b -f -p /usr/local
+
+    echo "Installing SnapML packages"
+    echo "Please standby, this will take a few minutes..."
+    # install RAPIDS packages
+    !conda config --prepend channels \
+https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+    conda config --set channel_priority strict
+    conda install -y --prefix /usr/local \
+      python=3.6 cudatoolkit=10.2 \
+      pai4sk seaborn matlib scikit-learn
+    echo "Copying shared object files to /usr/lib"
+    # copy .so files to /usr/lib, where Colab's Python looks for libs
+    cp /usr/local/lib/lib*.so /usr/lib/
+fi
+
+echo ""
+echo "*********************************************"
+echo "Your Google Colab instance is SnapML ready!"
+echo "*********************************************"
+
+~                                                                                                                                                                                                           
+~                                                                     
